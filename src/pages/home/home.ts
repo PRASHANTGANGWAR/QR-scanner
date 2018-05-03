92a55244cc6186ff35da7c21800f385291ba312f
prashant gangwar <prashant.enbake@gmail.com>
2018-05-03 19:29:19 +0530

    added second scan

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,7 +12,9 @@ export class HomePage {
 
   products: any[] = [];
   selectedProduct: any;
+  selectedProduct2: any;
   productFound:boolean = false;
+  producNotFound:boolean = false;
   phone:any;
   otp:any;
 
@@ -27,17 +29,21 @@ export class HomePage {
         });
   }
 
-  scan() {
+  scanoutercode() {
     this.selectedProduct = {};
     this.barcodeScanner.scan().then((barcodeData) => {
       this.selectedProduct = this.products.find(product => product.plu === barcodeData.text);
       if(this.selectedProduct !== undefined) {
         this.productFound = true;
         console.log(this.selectedProduct);
+
+        this.scaninnercode();
+
       } else {
         this.selectedProduct = {};
         this.productFound = false;
-        this.toast.show('Product not found', '5000', 'center').subscribe(
+        this.producNotFound = true;
+        this.toast.show('not found', '5000', 'center').subscribe(
           toast => {
             console.log(toast);
           }
@@ -56,12 +62,39 @@ export class HomePage {
   login(phone,otp)
   {
     console.log(this.otp); console.log(this.phone);
-      if((this.phone == "9999999999") && (this.otp == "1234" || "undefined"))
+      if((this.phone == "9999999999" || "0000000000" || "1234") && (this.otp == "1234" || "undefined"))
        {
-     alert("login sucessful");
-     this.scan();
+     //alert("login sucessful");
+     this.scanoutercode();
        }
  }
   
+
+  scaninnercode()
+  {
+      this.selectedProduct2 = {};
+    this.barcodeScanner.scan().then((barcodeData) => {
+      this.selectedProduct2 = this.products.find(product => product.plu === barcodeData.text);
+      if(this.selectedProduct !== undefined) {
+        this.productFound = true;
+        console.log(this.selectedProduct);
+      } else {
+        this.selectedProduct2 = {};
+        this.productFound = false;
+        this.toast.show('not found', '5000', 'center').subscribe(
+          toast => {
+            console.log(toast);
+          }
+        );
+      }
+    }, (err) => {
+      this.toast.show(err, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
+  
+  }
 
 }
